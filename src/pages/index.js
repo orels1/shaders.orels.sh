fdd8847dd33509fc5e1337308eae2fbe3f8d493a
orels1 <ping@orels.sh>
2022-04-18 04:26:48 +0400

    updated landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,8 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className={styles.heroBackground} />
+      <div className={clsx('container', styles.heroContainer)}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
@@ -32,9 +33,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
