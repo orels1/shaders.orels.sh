fc547b66da89d378a9cbaf8074c427ebfbeb742a
orels <ping@orels.sh>
2023-04-12 08:06:51 +0400

    made the whole AddToVCC button clickable

--- src/components/AddToVCC.tsx
+++ src/components/AddToVCC.tsx
@@ -2,7 +2,10 @@ const SHADERS_VCC_LINK = 'vcc://vpm/addRepo?url=https://orels1.github.io/orels-U
 
 export default function AddToVCC() {
   return (
-    <div className="relative group rounded-full border text-center border-orange-600 dark:border-amber-600">
+    <div
+      className="relative cursor-pointer group rounded-full border text-center border-orange-600 dark:border-amber-600"
+      onClick={() => window.location.assign(SHADERS_VCC_LINK)}
+    >
       <div className="absolute -inset-px rounded-full border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.red.400),theme(colors.amber.400),theme(colors.orange.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
       <button
         type="button"
