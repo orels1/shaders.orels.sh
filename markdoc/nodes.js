79dc9b742befde80587f6bb6ef543f8f32b85b3d
orels1 <ping@orels.sh>
2023-08-20 17:58:09 +0400

    adjusted markdoc image to render a title

--- markdoc/nodes.js
+++ markdoc/nodes.js
@@ -1,5 +1,7 @@
 import { nodes as defaultNodes } from '@markdoc/markdoc'
 import { Fence } from '@/components/Fence'
+import { Tag } from '@markdoc/markdoc';
+
 
 const nodes = {
   document: {
@@ -23,6 +25,26 @@ const nodes = {
       },
     },
   },
+  image: {
+    ...defaultNodes.image,
+    transform: (node, config) => {
+      const attributes = node.transformAttributes(config);
+      const children = node.transformChildren(config);
+  
+      return [
+        new Tag(
+          `img`,
+          { ...attributes, class: 'mb-2' },
+          children
+        ),
+        new Tag(
+          'span',
+          { class: 'text-sm text-center mt-4 mb-4 text-slate-400 block' },
+          [node.attributes.title]
+        )
+      ]
+    }
+  }
 }
 
 export default nodes
